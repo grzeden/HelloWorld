2e0695c1b901a5b649a3622143edfba4e5e91454
Dennis Grzegorzewski <my_email@email.com>
2016-01-18 20:39:03 -0600

    First Commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Dennis Grzegorzewski
+ * Date: 01/18/2016
+ * FileName: HelloWorld.java
+ * Homework: Practice1
+ */
+
+/**
+ * @author Dennis
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Output to console
+		System.out.println("Hello World!");
+	}
+
+}
